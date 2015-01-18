38a0079f949add1aea3c48943b34b2bbe27b9174
TianyiChen <TianyiChen@users.noreply.github.com>
2015-01-18 17:04:43 +0800

    Create dns.php

---
+++ dns.php
@@ -0,0 +1,11 @@
+<?php 
+$server=$_GET["q"]; 
+if (isset($server)) { 
+$hosts = gethostbynamel($server); 
+foreach ($hosts as $obj) {  
+   print "$obj;";  
+}  
+} else { 
+include "index_en.htm"; 
+ } 
+?>  
